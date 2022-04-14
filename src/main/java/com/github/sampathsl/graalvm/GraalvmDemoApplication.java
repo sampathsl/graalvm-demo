ecd34ca2c99370409b88185e6e1c409c4a3c01b8
Sampath Thennakoon <sampathde4@gmail.com>
2022-04-14 08:03:16 +0530

    Initial commit

---
+++ src/main/java/com/github/sampathsl/graalvm/GraalvmDemoApplication.java
@@ -0,0 +1,12 @@
+package com.github.sampathsl.graalvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraalvmDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GraalvmDemoApplication.class, args);
+  }
+}
---
+++ src/test/java/com/github/sampathsl/graalvm/GraalvmDemoApplicationTests.java
@@ -0,0 +1,11 @@
+package com.github.sampathsl.graalvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraalvmDemoApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
